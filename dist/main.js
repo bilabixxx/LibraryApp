6bd5bd1105df400cad828b730d40974a9e561d81
bilabixxx <gennuso.biagio@gmail.com>
2022-01-27 14:59:36 +0100

    Updated dist with webpack dev

--- dist/main.js
+++ dist/main.js
@@ -702,10 +702,10 @@ eval("module.exports = \"data:image/svg+xml,%3csvg xmlns=%27http://www.w3.org/20
 /*!******************************!*\
   !*** ./src/img/no-image.png ***!
   \******************************/
-/***/ ((module) => {
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
 
 "use strict";
-eval("module.exports = \"data:image/png;base64,bW9kdWxlLmV4cG9ydHMgPSBfX3dlYnBhY2tfcHVibGljX3BhdGhfXyArICJpbWdzL25vLWltYWdlLmY3ZTc1MDIzLnBuZyI7\";\n\n//# sourceURL=webpack://my-webpack-project/./src/img/no-image.png?");
+eval("module.exports = __webpack_require__.p + \"623f7df905c3aa74ced8.png\";\n\n//# sourceURL=webpack://my-webpack-project/./src/img/no-image.png?");
 
 /***/ })
 
@@ -801,6 +801,26 @@ eval("module.exports = \"data:image/png;base64,bW9kdWxlLmV4cG9ydHMgPSBfX3dlYnBhY
 /******/ 			if (!module.children) module.children = [];
 /******/ 			return module;
 /******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/publicPath */
+/******/ 	(() => {
+/******/ 		var scriptUrl;
+/******/ 		if (__webpack_require__.g.importScripts) scriptUrl = __webpack_require__.g.location + "";
+/******/ 		var document = __webpack_require__.g.document;
+/******/ 		if (!scriptUrl && document) {
+/******/ 			if (document.currentScript)
+/******/ 				scriptUrl = document.currentScript.src
+/******/ 			if (!scriptUrl) {
+/******/ 				var scripts = document.getElementsByTagName("script");
+/******/ 				if(scripts.length) scriptUrl = scripts[scripts.length - 1].src
+/******/ 			}
+/******/ 		}
+/******/ 		// When supporting browsers where an automatic publicPath is not supported you must specify an output.publicPath manually via configuration
+/******/ 		// or pass an empty string ("") and set the __webpack_public_path__ variable from your code to use your own logic.
+/******/ 		if (!scriptUrl) throw new Error("Automatic publicPath is not supported in this browser");
+/******/ 		scriptUrl = scriptUrl.replace(/#.*$/, "").replace(/\?.*$/, "").replace(/\/[^\/]+$/, "/");
+/******/ 		__webpack_require__.p = scriptUrl;
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/jsonp chunk loading */
